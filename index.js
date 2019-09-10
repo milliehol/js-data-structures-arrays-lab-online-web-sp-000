d0b32462d7f3b720b31032264a7c062e7e64532e
Carla Holcomb <carla.programmer@gmail.com>
2019-09-10 03:06:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+function appendDriver(name){
+  drivers.push(name);
+}
+function appendDriver(name){
+  drivers.push(name);
+}
+function removeLastDriver(){
+  drivers.push(name);
+}
+function removeFirstDriver(){
+  drivers.push();
+}
